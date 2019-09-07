a7626a6764cffbac4ad1910026ef78d203afab18
aboud <aboudiido@gmail.com>
2019-09-07 18:20:27 +0300

    Fix mixed content problem

--- src/apis/AccuWeather.js
+++ src/apis/AccuWeather.js
@@ -2,6 +2,6 @@ import axios from "axios";
 const KEY = "gHRpT9oFaGh88J1R6tMEMhKrvlCjNCyx";
 
 export default axios.create({
-  baseURL: "http://dataservice.accuweather.com"
+  baseURL: "https://dataservice.accuweather.com"
 });
 export { KEY };
