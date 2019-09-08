08a69c1a4bb35318dbd91d38e9adda88cca83969
aboud <aboudiido@gmail.com>
2019-09-08 12:02:48 +0300

    Updates

---
+++ precache-manifest.800e8ce950fa4d12f7f3c01b90364708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16548366bcaedf43f8bb671082836674",
+    "url": "/Ido-Aboudi-7-9-2019/index.html"
+  },
+  {
+    "revision": "421805c88c93508c6e97",
+    "url": "/Ido-Aboudi-7-9-2019/static/css/main.d414108a.chunk.css"
+  },
+  {
+    "revision": "75e73d16341cb2e7b37d",
+    "url": "/Ido-Aboudi-7-9-2019/static/js/2.71ea8825.chunk.js"
+  },
+  {
+    "revision": "421805c88c93508c6e97",
+    "url": "/Ido-Aboudi-7-9-2019/static/js/main.3a1f9dc9.chunk.js"
+  },
+  {
+    "revision": "0a983dad0fe13b8c0941",
+    "url": "/Ido-Aboudi-7-9-2019/static/js/runtime~main.418a0142.js"
+  }
+]);
